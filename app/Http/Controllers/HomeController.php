24e3a824c7569d2e4c221ae3929f8a9642016485
greisonsantos <greisonsantos03@gmail.com>
2019-02-19 09:31:02 -0300

    add post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
+use App\Post;
 
 class HomeController extends Controller
 {
@@ -24,6 +25,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+      $posts= Post::all();
+      return view('home', compact('posts'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,13 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+    @forelse($posts as $post)
+     <h1>{{$post->title}}</h1>
+      <p>{{$post->description}}</p>
+      <b> Authot: {{$post->user->name}}</b>
+      <hr>
+    @empty
+      <p>   Nenhum post foi emcontrado</p>
+    @endforelse
 </div>
 @endsection
